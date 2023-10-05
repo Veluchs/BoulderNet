ba2095801accb98f04a8ad8515aa00253ed72dc9
Veluchs <mysterioso@mailbox.org>
2023-10-05 12:09:20 +0200

    add function to convert polygon labels to mask
    the mask is created as image with different colors representing
    instances. mask is returned as numpy array

--- notebooks/1.0-dataset.py
+++ notebooks/1.0-dataset.py
@@ -18,6 +18,7 @@
 import os
 import numpy as np
 import torch
+from PIL import Image, ImageDraw
 
 
 # %%
@@ -28,11 +29,37 @@ class ClimbingHoldDataset(torch.utils.data.Dataset):
 
         # load image files, and labels
         self.imgs = list(sorted(os.path.join(root_dir, "images/")))
-        self.masks = list(sorted(os.path.join(root_dir, "labels/")))
+        self.labels = list(sorted(os.path.join(root_dir, "labels/")))
 
     def __len__(self):
         return len(self.imgs)
-    
+
+    def polygon_to_mask(self, label_path, img_path) -> np.array():
+        label = open(label_path)
+        lines = label.readLines()
+
+        image = Image.open(img_path)
+        width, height = image.size
+
+        # create empty mask with size of original image
+        mask_template = np.zeros([width, height])
+
+        mask = Image.new('RGB', mask_template.shape, 0)
+        mask_draw = ImageDraw.Draw(mask)
+
+        for i, line in enumerate(lines, 1):
+            polygon = np.fromstring(line[2:], sep=' ')
+            polygon_coordinates = [
+                int(polygon[2*i] * width), int(polygon[2*i + 1] * height)
+                for i in range(int(len(polygon)/2))
+                ]
+            mask_draw.polygon(polygon_coordinates, 
+                              outline=(i, 0, 0), 
+                              fill=(i, 0, 0)
+                             )
+            # TODO add overflow for more than 256 holds
+
+        return np.array(mask)
 
 
 # %%
