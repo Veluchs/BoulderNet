1eab33346794afcdad43a2002327913c24824205
Veluchs <mysterioso@mailbox.org>
2023-10-24 20:30:22 +0000

    add train and inference transforms

--- notebooks/2.0-model.py
+++ notebooks/2.0-model.py
@@ -3,18 +3,19 @@
 #   jupytext:
 #     cell_metadata_filter: -all
 #     custom_cell_magics: kql
+#     formats: ipynb,py
 #     text_representation:
 #       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.11.2
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.2
 #   kernelspec:
-#     display_name: bouldernet
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
-# %%
+# +
 import torchvision
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
@@ -70,7 +71,7 @@ def get_model_instance_segmentation(num_classes):
     return model
 
 
-# %%
+# +
 from torchvision import tv_tensors
 import sys
 
@@ -80,7 +81,7 @@ from dataset import ClimbingHoldDataset
 import torch
 from torchvision.models import MobileNet_V3_Large_Weights
 
-# %%
+# +
 from torchvision.transforms import v2
 
 transforms = v2.Compose([
@@ -93,7 +94,7 @@ transforms = v2.Compose([
 ])
 
 
-# %%
+# +
 
 dataset = ClimbingHoldDataset('../data/processed/', transforms=None)
 data_loader = torch.utils.data.DataLoader(
@@ -102,13 +103,13 @@ data_loader = torch.utils.data.DataLoader(
 
 
 
-# %%
+# +
 img = dataset.__getitem__(9)[0]
 target = dataset.__getitem__(9)[1]
 
 img.dtype
 
-# %%
+# +
 import math
 import sys
 import time
@@ -135,14 +136,14 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
         optimizer.zero_grad()
         losses.backward()
         optimizer.step()
-        print(i)
+        # print(i)
         if lr_scheduler is not None:
             lr_scheduler.step()
         epoch_loss += losses
     epoch_loss /= i+1
     return epoch_loss
 
-# %%
+# +
 import torch
 
 # some_file.py
@@ -154,14 +155,20 @@ from dataset import ClimbingHoldDataset
 
 from torchvision.transforms import v2
 
-transforms = v2.Compose([
-    v2.ToDtype(torch.uint8, scale=False),
-    v2.ColorJitter(brightness=0.3, contrast=0.1, saturation=0.4, hue=0.3),
-    v2.RandomRotation(180),
-    v2.ToDtype(torch.float),
-    v2.Normalize([0], [255]),
-    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
+
+def get_transform(train):
+    transforms = []
+    transforms.append(v2.ToDtype(torch.uint8, scale=False)
+    transforms.append(v2.ToDtype(torch.float32, scale=True)
+                      
+    if train:
+        transforms.append(v2.RandomHorizontalFlip(p=0.5))
+        transforms.append(v2.RandomVerticalFlip(p=0.5))
+        transforms.appen(v2.ColorJitter(brightness=0.3, contrast=0.1, saturation=0.4, hue=0.3))
+                      
+    transforms.append(v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]))
+
+    return v2.Compose(transforms)
 
 
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -171,15 +178,30 @@ device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cp
 model = get_model_instance_segmentation(3)
 model.to(device)
 # use our dataset and defined transformations
-dataset = ClimbingHoldDataset('../data/processed/', transforms)
+dataset = ClimbingHoldDataset('/datasets/holds/', get_transforms(train=True)
+dataset_test = ClimbingHoldDataset('/datasets/holds/', get_transforms(train=False)
 
-indices = range(len(dataset))
+
+
+indices = torch.randperm(len(dataset)).tolist()
 dataset = torch.utils.data.Subset(dataset, indices[:])
+dataset_test = torch.utils.data.Subset(dataset_test, indices[-5:])
 
-
+# define training and validation data loaders
 data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=1, shuffle=False, num_workers=1, collate_fn=utils.collate_fn
+    dataset,
+    batch_size=16,
+    shuffle=True,
+    num_workers=1,
+    collate_fn=utils.collate_fn
 )
+
+# data_loader_test = torch.utils.data.DataLoader(
+#     dataset_test,
+#     batch_size=1,
+#     shuffle=False,
+#     num_workers=4,
+#     collate_fn=utils.collate_fn
 
 
 # move model to the right device
@@ -195,10 +217,11 @@ lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
                                                gamma=0.1)
 
 # let's train it for 10 epochs
-num_epochs = 3
+num_epochs = 100
 
 for epoch in range(num_epochs):
     # train for one epoch, printing every 10 iterations
+    print(epoch)
     train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=1)
     # update the learning rate
     lr_scheduler.step()
@@ -206,23 +229,45 @@ for epoch in range(num_epochs):
 
 # print("That's it!")
 
-# %%
+# +
+from torchvision.transforms import v2
+sys.path.insert(1, '../src/')
+import utils
+
+
+
+
+inf_transforms = v2.Compose([
+    v2.ToDtype(torch.uint8, scale=False),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+
+
+
+dataset = ClimbingHoldDataset('/datasets/holds/', inf_transforms)
+
+
+img, target = dataset.__getitem__(1)
+
+
+# +
+# img, target = transforms(img, target)
+
+utils.show([v2.functional.to_dtype(img, torch.uint8, scale=True), target])
+
+# +
+
 model.eval()
-img = dataset.__getitem__(34)[0]
-target = dataset.__getitem__(34)[1]
-img.shape
+pred = model([img.to(device)])
+# -
 
-
-# %%
-pred = model([img])
-
-# %%
 img
 
-# %%
-pred
+pred[0]['masks'] = torch.squeeze(pred[0]['masks'])
 
-# %%
+# +
 from utils import show
 
-show([img.to(torch.uint8), pred[0]])
+show([img.to('cpu'), pred[0]], seg_mask=False)
