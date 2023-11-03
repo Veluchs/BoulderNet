4e6cba50afd7a7b3e99403fc337fe21cf9c4031d
Veluchs <mysterioso@mailbox.org>
2023-11-03 18:04:45 +0000

    fix missing masks
    if condition was never True

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -1,7 +1,6 @@
 import os
 import torch
 from PIL import Image, ImageOps
-import numpy as np
 import torchvision.transforms as T
 from torchvision.transforms import v2
 from torchvision import tv_tensors
@@ -34,21 +33,22 @@ def split(img, masks, labels):
     patches = tv_tensors.Image(patches)
 
     mask_patched = masks.unfold(1, KERNEL_SIZE, STRIDE).unfold(2,
-                                                                KERNEL_SIZE,
-                                                                STRIDE)
+                                                               KERNEL_SIZE,
+                                                               STRIDE)
     mask_patched = mask_patched.reshape(masks.shape[0], -1,
                                         KERNEL_SIZE, KERNEL_SIZE)
-
+  
     targets = []
     for patch_index, image_patch in enumerate(patches):
         patch_masks = []
         patch_labels = []
         for index, mask in enumerate(mask_patched):
-
-            if mask[patch_index].any() == True:
-                patch_masks.append(mask[patch_index]) 
-                patch_labels.append(labels[index]) # TODO better stacking
-
+            if torch.any(mask[patch_index]).item():
+                patch_masks.append(mask[patch_index])
+                patch_labels.append(labels[index])  # TODO better stacking
+        
+        # TODO potentially skip image if no holds
+    
         patch_masks = torch.stack(patch_masks)
         patch_target = {
             'masks': tv_tensors.Mask(patch_masks),
@@ -59,25 +59,26 @@ def split(img, masks, labels):
     return patches, targets
 
 
-
 IMAGE_RES = 768
-IMAGE_PATH = 'data/data-masks/img'
-MASK_PATH = 'data/data-masks/masks_instances'
+IMAGE_PATH = '../data/raw/img'
+MASK_PATH = '../data/raw/masks_instances'
 
 image_list = os.listdir(IMAGE_PATH)
 
 for image_file_name in image_list:
+    print(image_file_name)
     img = Image.open(os.path.join(IMAGE_PATH, image_file_name)).convert('RGB')
     img = ImageOps.exif_transpose(img)
     img = v2.functional.resize(img, size=IMAGE_RES)
 
     mask_folder = os.path.join(MASK_PATH, image_file_name[:-4])
     mask_list = os.listdir(mask_folder)
-
     masks = []
     labels = []
     for mask_filename in sorted(mask_list):
-        mask = torchvision.transforms.functional.pil_to_tensor(Image.open(os.path.join(mask_folder, mask_filename)))
+        mask = torchvision.transforms.functional.pil_to_tensor(
+            Image.open(os.path.join(mask_folder, mask_filename))
+            )
         mask = v2.functional.resize(mask, size=IMAGE_RES)
         masks.append(mask)
         if 'hold' in mask_filename:
@@ -88,7 +89,6 @@ for image_file_name in image_list:
             labels.append(2)
 
     masks = torch.squeeze(torch.stack(masks))
-
     patches, targets = split(img, masks, labels)
 
     transform = T.ToPILImage()
